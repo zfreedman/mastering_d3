7fc7b321c410b5a3374f542d0243f90cb1ffde6f
Zach Freedman <zfreedman92@gmail.com>
2018-08-23 16:08:19 -0400

    feat: Added http-server as a dev dependency
    Also provided documentation on how to use http-server to work with local files during development

--- 02_getting_started/app/src/components/app.js
+++ 02_getting_started/app/src/components/app.js
@@ -15,17 +15,25 @@ class App extends React.Component {
   }
 
   renderCircles () {
-    const data = [...Array(10).keys()].map(e => e * 2);
+    /*
+      READ READ READ READ READ READ READ READ READ READ READ
+
+      In order for this d3.csv call to work, make sure you to do the following:
+        1. npm install (make sure http-server is installed, dev dependency
+            for local files)
+        2. from another terminal/command line window, navigate to
+            app/
+        3. once in app/, run http-server ./ --cors="*"
+        4. the above step 3 will setup a server allowing CORS, with the root
+            located at app/ (so app/data/ages.csv is available)
+    */
+
+    const data = d3.csv("http://localhost:8080/data/ages.csv").then(data => {
+      console.log(data);
+    });
 
     const svg = d3.select("#canvas");
     const circles = svg.selectAll("circle").data(data);
-
-    circles.enter()
-      .append("circle")
-        .attr("cx", (d, i) => i * 25)
-        .attr("cy", d => d)
-        .attr("r", d => d)
-        .attr("fill", "purple");
   }
 }
 
