36a506ef7149d5201bb14817fbe49a66be97ceba
Zach Freedman <zfreedman92@gmail.com>
2018-08-23 13:56:54 -0400

    feat: Rendering an SVG canvas with a rect in it

---
+++ 02_getting_started/app/src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render () {
+    return (
+      <svg width="400" height="60">
+        <rect x="0" y="5" width="50" height="50" fill="green"></rect>
+      </svg>
+    );
+  }
+}
+
+export default App;
---
+++ 02_getting_started/app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "components/app";
+import "components/styles/app.css";
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+);
